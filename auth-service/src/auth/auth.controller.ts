5d713a367410bfa0011f94ccc232b49597904ef4
Joarez Ximenes <joarezximenesleitejunior@gmail.com>
2024-12-09 04:11:08 -0300

    feat: create auth service

--- auth-service/src/auth/auth.controller.ts
+++ auth-service/src/auth/auth.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { SignUpBodyDto } from './DTOs/signUpBodyDto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private authService: AuthService
+  ) {}
+
+  @Post('signup')
+  async signUp(@Body() body: SignUpBodyDto): Promise<void> {
+    await this.authService.createUser(body)
+    return;
+  }
+
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  async signIn(@Body() body: SignUpBodyDto): Promise<{ access_token: string}> {
+    const response = await this.authService.signIn(body);
+    return response;
+  }
+}
--- auth-service/src/auth/auth.service.ts
+++ auth-service/src/auth/auth.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from 'src/database/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService
+  ) {}
+
+  async createUser(data: Prisma.UserCreateInput) {
+    const hashPassword = await bcrypt.hash(data.password, 10);
+
+    const existentUser = await this.user({email: data.email})
+
+    if(existentUser) {
+      throw new ConflictException()
+    }
+
+    return this.prisma.user.create({ data: {
+      ...data,
+      password: hashPassword
+    } });
+  }
+
+  async user(
+    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+  ): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: userWhereUniqueInput,
+    });
+  }
+
+  async signIn(data: Prisma.UserCreateInput): Promise<{ access_token: string}> {
+    const user = await this.user({email: data.email});
+
+    if(!user) {
+      throw new NotFoundException('User not found')
+    }
+
+    const validPassword = await bcrypt.compare(data.password, user.password);
+
+    if(!validPassword) {
+      throw new UnauthorizedException('Invalid email or password')
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const payload = { sub: user.id }
+
+    return {access_token: await this.jwtService.signAsync(payload)};
+  }
+}
